279f4972d7c30469ba1141b184505fd0f320f202
Your Name <you@example.com>
2015-12-01 18:11:12 +0100

    running app w/update

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,61 +1,138 @@
+// Alloy.Global.currentLanguage = 'es';
+
 $.index.open();
 
 //input
+var titleLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:5000,
+	text:"GSA Calculator",
+	font:{
+		fontSize: '70.5sp',
+	}
+	
+});
+
+var swLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:100,
+	text:"SW",
+	
+});
 
 var swText = Titanium.UI.createTextField({
 
-	top:10,
-	left:10,
+	top:40,
+	left:30,
 	width:100,
 	
+	
+	
+});
+var frLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:100,
+	text:"FR",
 	
 });
 var frText = Titanium.UI.createTextField({
 
-	top:50,
-	left:10,
+	top:40,
+	left:30,
 	width:100,
+	
+});
+var hLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:100,
+	text:"H",
 	
 });
 var hText = Titanium.UI.createTextField({
 	
-	top:90,
-	left:10,
+	top:40,
+	left:30,
 	width:100,
+	
+});
+var imWLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:100,
+	text:"imW",
 	
 });
 var imWText = Titanium.UI.createTextField({
 	
-	top:140,
-	left:10,
+	top:40,
+	left:30,
 	width:100,
+	
+});
+var imHLabel = Titanium.UI.createLabel({
+
+	top:40,
+	left:30,
+	width:100,
+	text:"imH",
 	
 });
 var imHText = Titanium.UI.createTextField({
 	
-	top:190,
-	left:10,
+	top:40,
+	left:30,
 	width:100,
 	
 });
 
-//labels
+
+var resetButton = Titanium.UI.createButton({
+	top:40,
+	left:30,
+	width:120,
+	title:"Reset",
+	
+});
+
+// var str = Ti.Locale.getString('faivederestolanguage' , 'No traslation avaible');
+
+/*var esLanguageButton = Titanium.UI.createButton({
+	top:40,
+	left:10,
+	width:120,
+	title: "EsLanguages",
+	
+	
+});*/
+
+
+//output
 
 var gsaLabel = Titanium.UI.createLabel({
-	top:240,
+	top:40,
 	left:10,
 	width:150,
+	
 	
 });
 
 var dwLabel = Titanium.UI.createLabel({
-	top:290,
+	top:40,
 	left:10,
 	width:150,
 	
 });
 var dhLabel = Titanium.UI.createLabel({
-	top:340,
+	top:40,
 	left:10,
 	width:150,
 	
@@ -88,7 +165,29 @@ imHText.addEventListener('change', function(){
 	calculateDW();
 	calculateDH();
 });
+resetButton.addEventListener('click', function(){
+	
+	swText.value = 0;
+	frText.value = 0;
+	hText.value = 0;
+	imWText.value = 0;
+	imHText.value = 0;
+	gsaLabel.text = "";
+	dwLabel.text = "";
+	dhLabel.text = "";
+	
+	Ti.App.fireEvent('resettingFields');
+});
 
+
+Ti.App.addEventListener('resettingFields', function(){
+	
+	
+	});
+
+/*esLanguageButton.addEventListener('click', function(){
+	alert(str);
+});*/
 
 var eventFunction = function(input, color){
 	input.addEventListener('focus', function(){
@@ -106,37 +205,69 @@ eventFunction(imHText, 'yellow');
 
 
 // add 
-
+$.index.add(titleLabel);
+$.index.add(swLabel);
 $.index.add(swText);
+$.index.add(frLabel);
 $.index.add(frText);
+$.index.add(hLabel);
 $.index.add(hText);
+$.index.add(imWLabel);
 $.index.add(imWText);
+$.index.add(imHLabel);
 $.index.add(imHText);
 $.index.add(gsaLabel);
 $.index.add(dwLabel);
 $.index.add(dhLabel);
+$.index.add(resetButton);
+
+// $.index.add(esLanguageButton);
 
 
 
-// declaration mode
+// functions
+
 var place;
+var calc;
+var calctwo;
 
 function calculateGsa(e) {
-    gsaLabel.text =  (Math.round((swText.value * hText.value * 100)/(frText.value * imWText.value)) ? Math.round((swText.value * hText.value * 100)/(frText.value * imWText.value)) : 1 );
+	
+    gsaLabel.text =(frText.value && imWText.value) ? ((Math.round((swText.value * hText.value * 100)/(frText.value * imWText.value)) ? Math.round((swText.value * hText.value * 100)/(frText.value * imWText.value)) : 1 )) : ' ';
     place = gsaLabel.text;
-    gsaLabel.text = "GSAcalculator" + place;   
+    gsaLabel.text = "GSAcalculator: " + place;   
 };
 function calculateDW(e) {
 
-    dwLabel.text = "DWcalculator: " + Math.round((place * imWText.value)/100);
-   
+    dwLabel.text = imWText.value ? "DWcalculator: " + Math.round((place * imWText.value)/100) : ' ';
+    
 };
 function calculateDH(e) {
 	
 
-    dhLabel.text = "DHcalculator: " + Math.round((place * imHText.value )/100);
-   
+    dhLabel.text = imHText.value ? "DHcalculator: " + Math.round((place * imHText.value )/100) : ' ';
 };
+
+
+/*function reset(){
+
+	swText.text = 0;
+	frText.text = 0;
+	hText.text = 0;
+	imWText.text = 0;
+	imHText.text = 0;
+	Ti.App.fireEvent('resettingFields');
+	
+};
+
+
+Ti.App.addEventListener('resettingFields', function(){
+	
+	resetButton.addEventListener('click', function(){
+		reset();
+	});
+	
+});*/
 
 
 
